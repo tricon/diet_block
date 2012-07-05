db28c7c929e7c1bf728dc5c3dddeec3c94b22ef8
David Aaron Fendley <tricon@me.com>
2012-07-04 22:55:58 -0500

    Added stricter validations to the Meal model.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,4 +4,7 @@ class Meal < ActiveRecord::Base
   belongs_to :meal_aspect
 
   validates_presence_of :meal_type_id, :message => 'must belong to a meal type'
+  validates_presence_of :meal_aspct_id, :message => 'must belong to a meal aspect'
+  validates_format_of :meal_type_id, :with => /^[\d]+$/, :message => 'must be a number'
+  validates_format_of :meal_aspect_id, :with => /^[\d]+$/, :message => 'must be a number'
 end
